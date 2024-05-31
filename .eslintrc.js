0b60557a9dcfbccd318e5cd0686718e8ad4bf703
Anton <urazaev.tosha@yandex.ru>
2024-05-31 23:58:16 +0400

    add jest + test & add babel + plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
 		'no-underscore-dangle': 'off',
 		'object-curly-spacing': ['error', 'always'],
 		'i18next/no-literal-string': ['error', { markupOnly: true }],
+		'max-len': ['error', { ignoreComments: true }],
 	},
 	globals: {
 		__IS_DEV__: true,
