c60d8d76c71b7e420379e7440dcfc2e033602464
Anton <urazaev.tosha@yandex.ru>
2024-06-01 00:06:07 +0400

    fix codeStyle

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
 		'no-underscore-dangle': 'off',
 		'object-curly-spacing': ['error', 'always'],
 		'i18next/no-literal-string': ['error', { markupOnly: true }],
-		'max-len': ['error', { ignoreComments: true }],
+		'max-len': ['error', { code: 100, ignoreComments: true }],
 	},
 	globals: {
 		__IS_DEV__: true,
