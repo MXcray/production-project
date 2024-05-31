91d1ad84f7d02bedaf61a43a4bb9386fc16ad9d9
Anton <urazaev.tosha@yandex.ru>
2024-06-01 01:54:13 +0400

    add notFoundPage logic & loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,24 +1,25 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from "react-router-dom";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { PageLoader } from "widgets/PageLoader";
 
 const AppRouter = () => {
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<Routes>
-				{Object.values(routeConfig).map(({ element, path }) => (
-					<Route
-						key={path}
-						path={path}
-						element={
-							<div className={'page-wrapper'}>
+		<Routes>
+			{Object.values(routeConfig).map(({ element, path }) => (
+				<Route
+					key={path}
+					path={path}
+					element={
+						<div className={'page-wrapper'}>
+							<Suspense fallback={<div><PageLoader /></div>}>
 								{element}
-							</div>
-						}
-					/>
-				))}
-			</Routes>
-		</Suspense>
+							</Suspense>
+						</div>
+					}
+				/>
+			))}
+		</Routes>
 	);
 };
 
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,40 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import './Loader.scss';
+
+interface LoaderProps {
+	className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => {
+
+	return (
+		<div className={classNames('loadingio-spinner-spin-nq4q5u6dq7r', {}, [className])}>
+			<div className="ldio-x2uulkbinbj">
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+			</div>
+		</div>
+	);
+};
