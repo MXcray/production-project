04265145e65b9f08018eb5175181f5e76c40f0d6
Anton <urazaev.tosha@yandex.ru>
2024-09-03 08:52:36 +0400

    update app router

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,13 +4,14 @@ import { AppRouter } from "app/providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
 import { Suspense, useEffect } from 'react';
-import { useDispatch } from "react-redux";
-import { userActions } from "entities/User";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserInited, userActions } from "entities/User";
 
 const App = () => {
 
 	const { theme } = useTheme();
 	const dispatch = useDispatch();
+	const inited = useSelector(getUserInited);
 
 	useEffect(() => {
 		dispatch(userActions.initAuthData());
@@ -22,7 +23,7 @@ const App = () => {
 				<Navbar className={'navbar'} />
 				<div className={'content-page'}>
 					<Sidebar />
-					<AppRouter />
+					{inited && <AppRouter />}
 				</div>
 			</Suspense>
 		</div>
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,39 +1,31 @@
-import React, { memo, Suspense, useMemo } from 'react';
+import React, { memo, Suspense, useCallback } from 'react';
 import { Route, Routes } from "react-router-dom";
-import { routeConfig } from "shared/config/routeConfig/routeConfig";
 import { PageLoader } from "widgets/PageLoader";
-import { useSelector } from "react-redux";
-import { getUserAuthData } from "entities/User";
+import { appRoutesProps, routeConfig } from "shared/config/routeConfig/routeConfig";
+import { RequireAuth } from "app/providers/router/ui/RequireAuth";
 
 const AppRouter = () => {
 
-	const isAuth = useSelector(getUserAuthData);
-
-	const routes = useMemo(() => {
-		return Object.values(routeConfig).filter(route => {
-			if (route.authOnly && !isAuth) {
-				return false;
-			}
-
-			return  true;
-		})
-	}, [isAuth])
+	const renderWithWrapper = useCallback((route: appRoutesProps) => {
+		const element = (
+			<div className={'page-wrapper'}>
+				<Suspense fallback={<div><PageLoader/></div>}>
+					{route.element}
+				</Suspense>
+			</div>
+		)
+		return (
+			<Route
+				key={route.path}
+				path={route.path}
+				element={route.authOnly ? <RequireAuth>{element}</RequireAuth> : element}
+			/>
+		)
+	}, [])
 
 	return (
 		<Routes>
-			{routes.map(({ element, path }) => (
-				<Route
-					key={path}
-					path={path}
-					element={
-						<div className={'page-wrapper'}>
-							<Suspense fallback={<div><PageLoader /></div>}>
-								{element}
-							</Suspense>
-						</div>
-					}
-				/>
-			))}
+			{Object.values(routeConfig).map(renderWithWrapper)}
 		</Routes>
 	);
 };
