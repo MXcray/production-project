562c19db5a2fc186be0e14020c4ffccd5fd805f6
Anton <urazaev.tosha@yandex.ru>
2024-11-14 19:16:42 +0400

    add private route with required roles

--- src/app/providers/router/ui/RequireAuth.tsx
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -1,14 +1,36 @@
 import { useSelector } from "react-redux";
-import { getUserAuthData } from "entities/User";
+import { getUserAuthData, getUserRoles } from "entities/User";
 import { Navigate, useLocation } from "react-router-dom";
 import { RoutePath } from "shared/config/routeConfig/routeConfig";
+import { UserRole } from "entities/User/model/types/user";
+import { useMemo } from "react";
 
-export function RequireAuth({ children }: {children: JSX.Element}) {
+interface RequireAuthProps {
+	children: JSX.Element;
+	roles?: UserRole[];
+}
+
+export function RequireAuth({ children, roles }: RequireAuthProps) {
 	let auth = useSelector(getUserAuthData);
 	let location = useLocation();
+	const userRoles = useSelector(getUserRoles);
+
+	const hasRequiredRoles = useMemo(() => {
+		if (!roles) {
+			return true;
+		}
+
+		return roles.some(requiredRole => {
+			return userRoles?.includes(requiredRole);
+		})
+	}, [roles, userRoles]);
 
 	if (!auth) {
 		return <Navigate to={RoutePath.main} state={{ from: location }} replace />;
+	}
+
+	if (!hasRequiredRoles) {
+		return <Navigate to={RoutePath.forbidden} state={{ from: location }} replace />;
 	}
 
 	return children;
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -2,6 +2,7 @@ import { userReducer, userActions } from "./model/slice/userSlice";
 import { User, UserSchema } from './model/types/user';
 import { getUserAuthData } from "./model/selectors/getUserAuthData/getUserAuthData";
 import { getUserInited } from "./model/selectors/getUserInited/getUserInited";
+import { getUserRoles, isUserAdmin, isUserManager } from "./model/selectors/roleSelector";
 
 export {
 	userReducer,
@@ -9,5 +10,8 @@ export {
 	User,
 	UserSchema,
 	getUserAuthData,
-	getUserInited
+	getUserInited,
+	isUserAdmin,
+	isUserManager,
+	getUserRoles,
 }
