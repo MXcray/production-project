47f7fd12dca760a803a17ef56278b247fad7a72b
Anton <urazaev.tosha@yandex.ru>
2024-11-20 18:15:12 +0400

    add useModal hook, upgrade drawer

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -4,12 +4,14 @@ import { classNames, Mods } from "shared/lib/classNames/classNames";
 import cls from './Drawer.module.scss';
 import { Portal } from "shared/ui/Portal/Portal";
 import { Overlay } from "../Overlay/Overlay";
+import { useModal } from "shared/lib/hooks/useModal/useModal";
 
 interface DrawerProps {
 	className?: string;
 	children: ReactNode;
 	isOpen?: boolean;
 	onClose?: () => void;
+	lazy?: boolean;
 }
 
 export const Drawer = memo((props: DrawerProps) => {
@@ -18,18 +20,34 @@ export const Drawer = memo((props: DrawerProps) => {
 		children,
 		isOpen,
 		onClose,
+		lazy,
 	} = props;
 
 	const { theme } = useTheme();
 
+	const {
+		close,
+		isClosing,
+		isMounted,
+	} = useModal({
+		animationDelay: 300,
+		onClose,
+		isOpen,
+	});
+
 	const mods: Mods = {
 		[cls.opened]: isOpen,
+		[cls.isClosing]: isClosing,
+	}
+
+	if (lazy && !isMounted) {
+		return null;
 	}
 
 	return (
 		<Portal>
 			<div className={classNames(cls.Drawer, mods, [className, theme, 'app_drawer'])}>
-				<Overlay onClick={onClose}/>
+				<Overlay onClick={close}/>
 				<div
 					className={cls.content}
 				>
