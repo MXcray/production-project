5a9c5d3914bcb1415814d93557dac6e7c06b3070
Anton <urazaev.tosha@yandex.ru>
2024-12-04 16:43:41 +0400

    add appImage, fallback, errorFallback

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -15,6 +15,8 @@ import { AppLink } from "@/shared/ui/AppLink";
 import { ArticleView } from "../../model/consts/articleConsts";
 import { ArticleBlockType } from "../../model/consts/articleConsts";
 import { getRouteArticleDetails } from "@/shared/const/router";
+import { AppImage } from "@/shared/ui/AppImage";
+import { Skeleton } from "@/shared/ui/Skeleton";
 
 interface ArticleListItemProps {
 	className?: string;
@@ -61,7 +63,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
 					</div>
 					<Text title={article.title} className={cls.title}/>
 					{types}
-					<img src={article.img} alt={article.title} className={cls.img} />
+					<AppImage
+						fallback={<Skeleton width={'100%'} height={250} />}
+						src={article.img}
+						alt={article.title}
+						className={cls.img}
+					/>
 					{textBlock && (
 						<ArticleTextBlockComponent block={textBlock} className={cls.textBlock} />
 					)}
@@ -89,7 +96,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
 			className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
 			<Card className={cls.card}>
 				<div className={cls.imageWrapper}>
-					<img src={article.img} alt={article.title} className={cls.img}/>
+					<AppImage
+						fallback={<Skeleton width={200} height={200} />}
+						src={article.img}
+						alt={article.title}
+						className={cls.img}
+					/>
 					<Text text={article.createdAt} className={cls.date} />
 				</div>
 				<div className={cls.infoWrapper}>
--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,12 +1,17 @@
 import { classNames, Mods } from "@/shared/lib/classNames/classNames";
 import cls from './Avatar.module.scss';
 import { CSSProperties, useMemo } from "react";
+import { AppImage } from "../AppImage";
+import UserIcon from '../../assets/icons/user-filled.svg';
+import { Icon } from "../Icon";
+import { Skeleton } from "../Skeleton";
 
 interface AvatarProps {
 	className?: string;
 	src?: string;
 	size?: number;
 	alt?: string;
+	fallbackInverted?: boolean;
 }
 
 export const Avatar = (props: AvatarProps) => {
@@ -15,20 +20,26 @@ export const Avatar = (props: AvatarProps) => {
 		className,
 		src,
 		size,
-		alt
+		alt,
+		fallbackInverted,
 	} = props
 
 	const mods: Mods = {}
 
 	const styles = useMemo<CSSProperties>(() => {
 		return {
-			width: size || 100,
-			height: size || 100,
+			width: size,
+			height: size,
 		}
 	}, [size])
 
+	const fallback = <Skeleton width={size} height={size} borderRad={'50%'} />;
+	const errorFallback = <Icon inverted={fallbackInverted} width={size} height={size} Svg={UserIcon} />;
+
 	return (
-		<img
+		<AppImage
+			fallback={fallback}
+			errorFallback={errorFallback}
 			src={src}
 			style={styles}
 			className={classNames(cls.Avatar, mods, [className])}
