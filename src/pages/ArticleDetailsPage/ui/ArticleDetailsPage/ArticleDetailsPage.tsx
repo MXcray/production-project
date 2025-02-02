7f7d5a0699a7edb8198c9e6a4871c5ce2bce2560
Anton <urazaev.tosha@yandex.ru>
2025-02-02 23:15:22 +0400

    add edit widget, details container, plural forms

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -15,6 +15,10 @@ import { VStack } from '@/shared/ui/redesigned/stack';
 import { ArticleRecommendationsList } from '@/features/articleRecommendationList';
 import { ArticleDetailsComments } from '../../ui/ArticleDetailsComments/ArticleDetailsComments';
 import { ArticleRating } from '@/features/articleRaiting';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { StickyContentLayout } from '@/shared/layouts/StickyContentLayout';
+import { DetailsContainer } from '../DetailsContainer/DetailsContainer';
+import { AdditionalInfoContainer } from '../AdditionalInfoContainer/AdditionalInfoContainer';
 
 interface ArticleDetailsPageProps {
 	className?: string;
@@ -34,15 +38,37 @@ const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
 
 	return (
 		<DynamicModuleLoader reducers={reducers} removeAfterUnmount={true}>
-			<Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-				<VStack gap={'16'} max>
-					<ArticleDetailsPageHeader />
-					<ArticleDetails id={id} />
-					<ArticleRating articleId={id} />
-					<ArticleRecommendationsList />
-					<ArticleDetailsComments id={id} />
-				</VStack>
-			</Page>
+			<ToggleFeatures
+				feature={'isAppRedesigned'}
+				on={
+					<StickyContentLayout
+						content={
+							<Page
+								className={classNames(cls.ArticleDetailsPage, {}, [className])}
+							>
+								<VStack gap={'16'} max>
+									<DetailsContainer />
+									<ArticleRating articleId={id} />
+									<ArticleRecommendationsList />
+									<ArticleDetailsComments id={id} />
+								</VStack>
+							</Page>
+						}
+						right={<AdditionalInfoContainer />}
+					/>
+				}
+				off={
+					<Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+						<VStack gap={'16'} max>
+							<ArticleDetailsPageHeader />
+							<ArticleDetails id={id} />
+							<ArticleRating articleId={id} />
+							<ArticleRecommendationsList />
+							<ArticleDetailsComments id={id} />
+						</VStack>
+					</Page>
+				}
+			/>
 		</DynamicModuleLoader>
 	);
 };
