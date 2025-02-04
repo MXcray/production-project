99b9e2bfc4a4f7ea31aa9899deac1abcb1c92624
Anton <urazaev.tosha@yandex.ru>
2025-02-05 00:08:26 +0400

    redesign scroll

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -24,6 +24,10 @@ const ThemeProvider = (props: ThemeProviderProps) => {
 		}
 	}, [isThemeInited, defaultTheme]);
 
+	useEffect(() => {
+		document.body.className = theme;
+	}, [theme]);
+
 	const defaultProps = useMemo(
 		() => ({
 			theme: theme,
